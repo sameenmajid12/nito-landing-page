a04b729b95f07251d1784694a21f2f4564880185
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-09-24 20:14:27 -0400

    Implement header component and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -9,11 +7,51 @@ import Features from "./components/Features";
 import Footer from "./components/Footer";
 import JoinWaitlist from "./components/JoinWaitlist";
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [active, setActive] = useState("home");
+  const [windowTop, setWindowTop] = useState();
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActive(entry.target.id);
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+    sections.forEach((section) => observer.observe(section));
+    return () => {
+      sections.forEach((section) => observer.unobserve(section));
+    };
+  }, []);
+  useEffect(() => {
+    const handleScrollTop = () => {
+      setWindowTop(window.scrollY === 0);
+    };
+    window.addEventListener("scroll", handleScrollTop);
+    return () => {
+      window.removeEventListener("scroll", handleScrollTop);
+    };
+  }, []);
+  const checkActive = (route) => active === route;
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (!checkActive(sectionId)) {
+      window.scrollTo({
+        top: section.offsetTop - 72,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
-    <div>
-      <Header />
+    <div className="main-wrapper">
+      <Header
+        checkActive={checkActive}
+        scrollToSection={scrollToSection}
+        windowTop={windowTop}
+      />
       <Hero />
       <HowItWorks />
       <Features />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,84 +1,48 @@
-import { useEffect, useState } from "react";
-import "../index.css";
-import HeaderMenu from "./HeaderMenu";
-function Header({ windowTop, scrollToSection, checkActive, changeThemeColor }) {
-  const [menuVisibility, setMenuVisibility] = useState(false);
-  useEffect(() => {
-    if (windowTop) {
-      changeThemeColor(menuVisibility ? "#fff" : "#efefef");
-    }
-  }, [menuVisibility]);
-  const toggleMenu = () => {
-    setMenuVisibility((prev) => !prev);
-  };
-  const selectorLine = (
-    <span className="animate-slide-up bottom-[-5px] right-1/2 transform -translate-x-[-50%] absolute w-3.75 h-0.75 rounded bg-black"></span>
-  );
+import { useState } from "react";
+import "../styles/header.css";
+function Header({ windowTop, checkActive, scrollToSection }) {
+  const activeLine = <span className="active-line" />;
   return (
-    <>
-      <div
-        className={`flex justify-between sidePadding h-18 items-center w-[100dvw] ${
-          !windowTop
-            ? "bg-white shadow-md fixed top-0 left-0 z-110"
-            : "absolute top-0 left-0 z-110 shadow-none bg-transparent"
-        }`}
-      >
-        <img
-          className="w-auto h-[clamp(1.25rem,1.5vw,1.5rem)]"
-          src="/assets/logo.png"
-        ></img>
-        <div className="text-primary hidden md:flex text-l gap-x-5">
-          <div
-            className={`relative cursor-pointer  h-fit ${
-              checkActive("home") ? "font-bold" : ""
-            }`}
+    <div className="header-wrapper">
+      <img className="logo" src="/assets/logo.png" />
+      <div className="header-right">
+        <div className="header-link-wrapper">
+          <a
+            onClick={() => scrollToSection("home")}
+            className={
+              checkActive("home") ? "header-link-active" : "header-link"
+            }
           >
-            <a onClick={() => scrollToSection("home")}>Home</a>
-
-            {checkActive("home") ? selectorLine : ""}
-          </div>
-          <div
-            className={`relative cursor-pointer h-fit ${
-              checkActive("experience") ? "font-bold" : ""
-            }`}
-          >
-            <a onClick={() => scrollToSection("experience")}>Experience</a>
-            {checkActive("experience") ? selectorLine : ""}
-          </div>
-          <div
-            className={` relative cursor-pointer  h-fit ${
-              checkActive("projects") ? "font-bold" : ""
-            }`}
-          >
-            <a onClick={() => scrollToSection("projects")}>Projects</a>
-
-            {checkActive("projects") ? selectorLine : ""}
-          </div>
-          <div
-            className={`relative cursor-pointer  h-fit ${
-              checkActive("skills") ? "font-bold" : ""
-            }`}
-          >
-            <a onClick={() => scrollToSection("skills")}>Skills</a>
-            {checkActive("skills") ? selectorLine : ""}
-          </div>
-          <button className="bg-primary text-white w-[clamp(3rem,10vw,7rem)] h-[clamp(2.125rem,3vw,2.5rem)] rounded-full">
-            Blog
-          </button>
+            Home
+          </a>
+          {checkActive("home") && activeLine}
         </div>
-
-        <div className="cursor-pointer inline md:hidden text-xl text-[#313131] hover:text-[#6F6F6F]">
-          <i onClick={toggleMenu} className="fa-solid fa-bars"></i>
+        <div className="header-link-wrapper">
+          <a
+            onClick={() => scrollToSection("howitworks")}
+            className={
+              checkActive("howitworks") ? "header-link-active" : "header-link"
+            }
+          >
+            How it works
+          </a>
+          {checkActive("howitworks") && activeLine}
         </div>
+        <div className="header-link-wrapper">
+          <a
+            onClick={() => scrollToSection("features")}
+            className={
+              checkActive("features") ? "header-link-active" : "header-link"
+            }
+          >
+            Features
+          </a>
+          {checkActive("features") && activeLine}
+        </div>
+        <button className="waitlist-button">Join the waitlist</button>
       </div>
-      {menuVisibility && (
-        <HeaderMenu
-          toggleVisibility={toggleMenu}
-          scrollToSection={scrollToSection}
-          checkActive={checkActive}
-        />
-      )}
-    </>
+    </div>
   );
 }
+
 export default Header;
